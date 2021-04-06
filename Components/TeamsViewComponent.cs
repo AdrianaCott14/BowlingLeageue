ba336f527251f90b6fd6d63d025921ccf9b6819e
Adriana Cottle <adriana.cottle@gmail.com>
2021-04-05 22:25:03 -0600

    adding project

---
+++ Components/TeamsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Components
+{
+    public class TeamsViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        //constructor
+        public TeamsViewComponent(BowlingLeagueContext con)
+        {
+            context = con;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedTeam = RouteData?.Values["teamid"];
+
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
